3e7dc617c8364f30b6e1fd605d43827ed0b0bc42
Do Minh Phong <dominhphong306@gmail.com>
2023-03-15 07:52:57 +0700

    feat: update blockchain program

--- blockchain/programs/Rent/src/lib.rs
+++ blockchain/programs/Rent/src/lib.rs
@@ -14,21 +14,8 @@ pub mod errors;
 #[program]
 pub mod rent {
     use super::*;
-    pub fn list_item(ctx: Context<ListItem>, price: u64, rental_period: u64, is_continue_list: u8)
-        -> Result<()> {
-        list_item::exec(ctx,  price, rental_period, is_continue_list)
-    }
-
-    pub fn claim(ctx: Context<Claim>) -> Result<()> {
-        claim::exec(ctx)
-    }
-
-    pub fn rent(ctx: Context<RentItem>) -> Result<()> {
-        rent_item::exec(ctx)
-    }
-
-    pub fn update_item(ctx: Context<UpdateItem>, price: u64, rental_period: u64, is_continue_list: u8) -> Result<()> {
-        update_item::exec(ctx, price, rental_period, is_continue_list)
+    pub fn create_nft_entry(ctx: Context<CreateNFT>) -> ProgramResult {
+        create_nft(ctx, ctx.accounts.name.to_string(), ctx.accounts.description.to_string(), ctx.accounts.image.to_string())
     }
 }
 
